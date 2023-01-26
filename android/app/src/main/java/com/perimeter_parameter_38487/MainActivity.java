cb57817660f5bb1141a30c37ebd0713c35880ee9
Crowdbotics <team@crowdbotics.com>
2023-01-26 13:55:33 +0000

    Initial commit.

---
+++ android/app/src/main/java/com/perimeter_parameter_38487/MainActivity.java
@@ -0,0 +1,15 @@
+package com.perimeter_parameter_38487;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+  /**
+   * Returns the name of the main component registered from JavaScript. This is used to schedule
+   * rendering of the component.
+   */
+  @Override
+  protected String getMainComponentName() {
+    return "perimeter_parameter_38487";
+  }
+}
---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "perimeter-parameter-38487",
+  url: "https://perimeter-parameter-38487.botics.co",
+  api: "https://perimeter-parameter-38487.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
